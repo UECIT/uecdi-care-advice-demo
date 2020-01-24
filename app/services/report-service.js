0ed42fcdc912892b18dc62c8099eaa7cff34e7c6
Charlie Marcus <charlie.marcus1@nhs.net>
2020-01-24 13:10:09 +0000

    Feature/ncth 345 extra fields

--- app/services/report-service.js
+++ app/services/report-service.js
@@ -4,8 +4,12 @@ const Fhir = require('fhir').Fhir;
 const FhirVersions = require('fhir').Versions;
 const ParseConformance = require('fhir').ParseConformance;
 
+const newValueSets = JSON.parse(fs.readFileSync('app/assets/files/valuesets.json').toString());
+const newTypes = JSON.parse(fs.readFileSync('app/assets/files/profiles-types.json').toString());
 const newResources = JSON.parse(fs.readFileSync('app/assets/files/profiles-resources.json').toString());
 const parser = new ParseConformance(false, FhirVersions.STU3);
+parser.parseBundle(newValueSets);
+parser.parseBundle(newTypes);
 parser.parseBundle(newResources);
 
 const fhir = new Fhir(parser);
